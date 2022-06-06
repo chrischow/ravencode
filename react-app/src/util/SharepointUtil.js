b9d1ad9982acad86248eb55e45dfaf6b037141ed
EC2 Default User <ec2-user@ip-172-31-81-146.ec2.internal>
2022-06-06 08:31:49 +0000

    draft SP utils

--- react-app/src/util/SharepointUtil.js
+++ react-app/src/util/SharepointUtil.js
@@ -1,3 +1,5 @@
+import $ from 'jquery';
+
 export class RavencodeFolderData{
     constructor(id, dataName, type, path){
         this.id = id;
@@ -21,20 +23,53 @@ export class SharepointUtil{
     constructor(siteUrl) {
         this.siteUrl = siteUrl
     }
+    
+    /**
+     * FOLDER --
+     * _api/web/folders
+     * d.results[].__metadata.uri /_api/web/GetFolderByServerRelativeUrl('')
+     * d.results[].__metadata.type = SP.Folder or SP.File
+     * d.results[].Name
+     * d.results[].ServerRelativeUrl /xxx/xxx
+     * d.results[].ItemCount 0 means empty folder
+     * 
+     * FILE --
+     * _api/web/files
+     * d.results[].__metadata.uri /_api/web/GetFileByServerRelativeUrl('')
+     * d.results[].__metadata.type = SP.Folder or SP.File
+     * d.results[].Name
+     * d.results[].ServerRelativeUrl /xxx/xxx/xxx.xxx
+     * 
+     * FILE Data--
+     * headers "X-FORMS_BASE_AUTH_ACCEPTED", "f"
+     * text returned. responseText
+     * _api/web/GetFileByServerRelativeUrl('')/$value
+     */
+    getSPFolders(callback) {
+        const reqDigest = $.ajax({
+            url: new URL("_api/web/folders", this.siteUrl),
+            method: 'GET',
+            async: true,
+            headers: {
+                'Accept': 'application/json;odata=verbose'
+            },
+            success: data => callback(data),
+            error: (err) => console.log(JSON.stringify(err))
+        })
+    }
 
     getXRequestDigest() {
-        const url = new URL("_api/contextinfo", this.siteUrl);
-        const options = {
+        const reqDigest = $.ajax({
+            url: new URL("_api/contextinfo", this.siteUrl),
             method: 'POST',
+            async: false,
             headers: {
-                'Content-Type': 'application/json',
                 'Accept': 'application/json;odata=verbose'
-            }
-        }
-        const xhr = new XMLHttpRequest();
-        Object.entries(options.headers).map((x) => xhr.setRequestHeader(x[0], x[1]));
-        xhr.open(options.method, url, false);
-        return xhr.send().d.FormDigestValue;
+            },
+            success: data => data,
+            error: (err) => console.log(JSON.stringify(err))
+        })
+        return reqDigest.responseJSON.d.GetContextWebInformation.FormDigestValue;
     }
 
     static folderData(folderPath) {
